4b0aa2cc058add170a60db80a4c42a8509fe8e2c
unknown <arne.n.bjerkaas@gmail.com>
2021-07-03 12:37:56 +0200

    Fix counter

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ let squareMatrix = [
 let gameComplete = false;
 let playerOneCounter = 0;
 let playerTwoCounter = 0;
-let pointGiven = false;
 
 restart.addEventListener("click", () => {
   console.log("restart clicked");
@@ -50,12 +49,13 @@ restart.addEventListener("click", () => {
 // X and O icons
 
 Array.from(squares).forEach((square, index) => {
-  let playerOnePointGiven = false;
-  let playerTwoPointGiven = false;
   square.addEventListener("click", () => {
     console.log("squares :", square, "index: ", index);
     console.log("playerOneTurn: ", playerOneTurn);
     console.log("playerTwoTurn: ", playerTwoTurn);
+    let playerOnePointGiven = false;
+    let playerTwoPointGiven = false;
+    let pointGiven = false;
 
     // Make sure you cannot click already taken square
     if (
@@ -131,90 +131,53 @@ Array.from(squares).forEach((square, index) => {
     // Check for winner
     // PlayerOne - Pink
     if (
-      firstRow.every((current) => pinkArray.includes(current)) ||
-      secondRow.every((current) => pinkArray.includes(current)) ||
-      thirdRow.every((current) => pinkArray.includes(current)) ||
-      firstColumn.every((current) => pinkArray.includes(current)) ||
-      secondColumn.every((current) => pinkArray.includes(current)) ||
-      thirdColumn.every((current) => pinkArray.includes(current)) ||
-      diagonalFromLeft.every((current) => pinkArray.includes(current)) ||
-      diagonalFromRight.every((current) => pinkArray.includes(current))
+      (firstRow.every((current) => pinkArray.includes(current)) ||
+        secondRow.every((current) => pinkArray.includes(current)) ||
+        thirdRow.every((current) => pinkArray.includes(current)) ||
+        firstColumn.every((current) => pinkArray.includes(current)) ||
+        secondColumn.every((current) => pinkArray.includes(current)) ||
+        thirdColumn.every((current) => pinkArray.includes(current)) ||
+        diagonalFromLeft.every((current) => pinkArray.includes(current)) ||
+        diagonalFromRight.every((current) => pinkArray.includes(current))) &&
+      !playerOnePointGiven &&
+      !pointGiven &&
+      !gameComplete
     ) {
       console.log("PINK is tha winna");
 
       // make sure only one point is given per round
-      if (pinkArray.length == 3 && !playerOnePointGiven) {
-        console.log("pink lenght and no point given: ", pinkArray.length);
-
-        playerOneCounter++;
-        // pointGiven = true;
-        playerOnePointGiven = true;
-        gameComplete = true;
-        console.log("pink point given: ", pointGiven);
-        // playerOnePointGiven = true;
-      }
-      /*       if (gameComplete) {
-        return;
-      }
-
+      console.log("pink lenght and no point given: ", pinkArray.length);
       playerOneCounter++;
-      gameComplete = true; */
-
-      // square.style = "pointer-events: none";
-
-      /*       Array.from(squares).forEach((el) => {
-        // el.style = "pointer-events: none";
-        console.log("EL pink: ", el);
-        if (el.getAttribute("style") == "background-color: pink;") {
-          el.style = "background-color: pink";
-        } else if (el.getAttribute("style") == "background-color: azure;") {
-          el.style = "background-color: azure";
-        }
-        el.style = "pointer-events: none";
-      }); */
-      //squares.style = "pointer-events: none";
-      // Set color back to what it was
+      playerOnePointGiven = true;
+      gameComplete = true;
+      console.log("pink point given: ", pointGiven);
+      pointGiven = true;
     }
 
     // PlayerTwo - Azure
     if (
-      firstRow.every((current) => azureArray.includes(current)) ||
-      secondRow.every((current) => azureArray.includes(current)) ||
-      thirdRow.every((current) => azureArray.includes(current)) ||
-      firstColumn.every((current) => azureArray.includes(current)) ||
-      secondColumn.every((current) => azureArray.includes(current)) ||
-      thirdColumn.every((current) => azureArray.includes(current)) ||
-      diagonalFromLeft.every((current) => azureArray.includes(current)) ||
-      diagonalFromRight.every((current) => azureArray.includes(current))
+      (firstRow.every((current) => azureArray.includes(current)) ||
+        secondRow.every((current) => azureArray.includes(current)) ||
+        thirdRow.every((current) => azureArray.includes(current)) ||
+        firstColumn.every((current) => azureArray.includes(current)) ||
+        secondColumn.every((current) => azureArray.includes(current)) ||
+        thirdColumn.every((current) => azureArray.includes(current)) ||
+        diagonalFromLeft.every((current) => azureArray.includes(current)) ||
+        diagonalFromRight.every((current) => azureArray.includes(current))) &&
+      !playerOnePointGiven &&
+      !pointGiven &&
+      !gameComplete
     ) {
       console.log("Azure is tha winna");
       // make sure only one point is given per round
-      if (azureArray.lengthh == 3 && !playerTwoPointGiven) {
-        console.log("azure lenght and no point given: ", azureArray.length);
 
-        playerTwoCounter++;
-        playerTwoPointGiven = true;
-        gameComplete = true;
-        console.log("azure point given: ", pointGiven);
-        // playerTwoPointGiven = true;
-      }
-      /*       if (gameComplete) {
-        return;
-      }
+      console.log("azure lenght and no point given: ", azureArray.length);
+
       playerTwoCounter++;
-      gameComplete = true; */
-      // square.style = "pointer-events: none";
-      /*       Array.from(squares).forEach((el) => {
-        // el.style = "pointer-events: none";
-        console.log("EL azre: ", el);
-        if (el.style == "background-color: pink") {
-          el.style = "background-color: pink";
-        } else if (el.style == "background-color: azure") {
-          el.style = "background-color: azure";
-        }
-        el.style = "pointer-events: none";
-      }); */
-      // Set color back to what it was
+      playerTwoPointGiven = true;
+      gameComplete = true;
+      console.log("azure point given: ", pointGiven);
+      pointGiven = true;
     }
 
     // Game over - no more
@@ -222,13 +185,23 @@ Array.from(squares).forEach((square, index) => {
       gameCompleteMessage.innerHTML = "Game Over";
       playerOneCounterText.innerHTML = playerOneCounter;
       playerTwoCounterText.innerHTML = playerTwoCounter;
-      // square.style = "pointer-events: none";
-      // Array.from(squares).forEach((el) => (el.style = "pointer-events: none"));
-      // playerOnePointGiven = true;
-      // playerTwoPointGiven = true;
+
       console.log("We have a winner - change style");
       gameComplete = false;
       pointGiven = false;
+
+      gameCompleteMessage.innerHTML = "";
+      squareMatrix = [
+        { index: 0, user: null },
+        { index: 1, user: null },
+        { index: 2, user: null },
+        { index: 3, user: null },
+        { index: 4, user: null },
+        { index: 5, user: null },
+        { index: 6, user: null },
+        { index: 7, user: null },
+        { index: 8, user: null },
+      ];
     }
   });
 });
