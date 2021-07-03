b1a3b3f963851fb82bdd7b4e34aeb603a3463619
unknown <arne.n.bjerkaas@gmail.com>
2021-07-03 11:26:23 +0200

    Working on counter

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ let squareMatrix = [
 let gameComplete = false;
 let playerOneCounter = 0;
 let playerTwoCounter = 0;
-
-let vsComputer = false;
+let pointGiven = false;
 
 restart.addEventListener("click", () => {
   console.log("restart clicked");
@@ -33,6 +32,7 @@ restart.addEventListener("click", () => {
     square.style = "background-color: cadetblue";
     console.log("test restart");
   });
+
   gameCompleteMessage.innerHTML = "";
   squareMatrix = [
     { index: 0, user: null },
@@ -49,23 +49,43 @@ restart.addEventListener("click", () => {
 
 // X and O icons
 
-// Add random computer mode
-// isComputerMode = false
-// Computer does first move - random
-// huge if -> if(computerMode) => playerTwo = Math.random()*8.floor
-
 Array.from(squares).forEach((square, index) => {
+  let playerOnePointGiven = false;
+  let playerTwoPointGiven = false;
   square.addEventListener("click", () => {
     console.log("squares :", square, "index: ", index);
+    console.log("playerOneTurn: ", playerOneTurn);
+    console.log("playerTwoTurn: ", playerTwoTurn);
 
+    // Make sure you cannot click already taken square
+    if (
+      squareMatrix[index].user == "playerOne" ||
+      squareMatrix[index].user == "playerTwo"
+    ) {
+      console.log("Square is TAKEN!");
+      console.log("squareMatrix[index].user", squareMatrix[index].user);
+      square.style = "pointer-events: none";
+      // Set color back to what it was
+      if (squareMatrix[index].user == "playerOne") {
+        square.style = "background-color: pink";
+      } else if (squareMatrix[index].user == "playerTwo") {
+        square.style = "background-color: azure";
+      }
+      return;
+    }
+
+    // turns
     if (playerOneTurn == true) {
+      // make sure you cant change colors
       if (square.getAttribute("style") == "background-color: azure;") {
         return;
       }
+
       square.style = "background-color: pink";
       squareMatrix[index].taken = true;
       squareMatrix[index].user = "playerOne";
     } else if (playerTwoTurn == true) {
+      // make sure you cant change colors
       if (square.getAttribute("style") == "background-color: pink;") {
         return;
       }
@@ -74,6 +94,8 @@ Array.from(squares).forEach((square, index) => {
       squareMatrix[index].user = "playerTwo";
     }
 
+    console.log("squareMatrix[index].user", squareMatrix[index].user);
+
     playerOneTurn = !playerOneTurn;
     playerTwoTurn = !playerTwoTurn;
 
@@ -81,6 +103,8 @@ Array.from(squares).forEach((square, index) => {
 
     let pinkArray = [];
     let azureArray = [];
+
+    // winner combos
     const firstRow = [0, 1, 2];
     const secondRow = [3, 4, 5];
     const thirdRow = [6, 7, 8];
@@ -101,84 +125,96 @@ Array.from(squares).forEach((square, index) => {
       }
     });
 
-    console.log("test array: ", pinkArray);
+    console.log("pink array: ", pinkArray);
+    console.log("azure array: ", azureArray);
 
+    // Check for winner
     // PlayerOne - Pink
-    if (firstRow.every((current) => pinkArray.includes(current))) {
-      console.log("PINK is tha winna - firstRow");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (secondRow.every((current) => pinkArray.includes(current))) {
-      console.log("PINK is tha winna - secondRow");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (thirdRow.every((current) => pinkArray.includes(current))) {
-      console.log("PINK is tha winna - thirdRow");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (firstColumn.every((current) => pinkArray.includes(current))) {
-      console.log("PINK is tha winna - firstColumn");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (secondColumn.every((current) => pinkArray.includes(current))) {
-      console.log("PINK is tha winna - secondColumn");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (thirdColumn.every((current) => pinkArray.includes(current))) {
-      console.log("PINK is tha winna - thirdColumn");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (
-      diagonalFromLeft.every((current) => pinkArray.includes(current))
-    ) {
-      console.log("PINK is tha winna - diagonalFromLeft");
-      gameComplete = true;
-      playerOneCounter++;
-    } else if (
+    if (
+      firstRow.every((current) => pinkArray.includes(current)) ||
+      secondRow.every((current) => pinkArray.includes(current)) ||
+      thirdRow.every((current) => pinkArray.includes(current)) ||
+      firstColumn.every((current) => pinkArray.includes(current)) ||
+      secondColumn.every((current) => pinkArray.includes(current)) ||
+      thirdColumn.every((current) => pinkArray.includes(current)) ||
+      diagonalFromLeft.every((current) => pinkArray.includes(current)) ||
       diagonalFromRight.every((current) => pinkArray.includes(current))
     ) {
-      console.log("PINK is tha winna - diagonalFromRight");
-      gameComplete = true;
+      console.log("PINK is tha winna");
+
+      // make sure only one point is given per round
+      if (pinkArray.length == 3 && !playerOnePointGiven) {
+        console.log("pink lenght and no point given: ", pinkArray.length);
+
+        playerOneCounter++;
+        // pointGiven = true;
+        playerOnePointGiven = true;
+        gameComplete = true;
+        console.log("pink point given: ", pointGiven);
+        // playerOnePointGiven = true;
+      }
+      /*       if (gameComplete) {
+        return;
+      }
+
       playerOneCounter++;
+      gameComplete = true; */
+
+      // square.style = "pointer-events: none";
+
+      /*       Array.from(squares).forEach((el) => {
+        // el.style = "pointer-events: none";
+        console.log("EL pink: ", el);
+        if (el.getAttribute("style") == "background-color: pink;") {
+          el.style = "background-color: pink";
+        } else if (el.getAttribute("style") == "background-color: azure;") {
+          el.style = "background-color: azure";
+        }
+        el.style = "pointer-events: none";
+      }); */
+      //squares.style = "pointer-events: none";
+      // Set color back to what it was
     }
 
     // PlayerTwo - Azure
-    if (firstRow.every((current) => azureArray.includes(current))) {
-      console.log("Azure is tha winna - firstRow");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (secondRow.every((current) => azureArray.includes(current))) {
-      console.log("Azure is tha winna - secondRow");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (thirdRow.every((current) => azureArray.includes(current))) {
-      console.log("Azure is tha winna - thirdRow");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (firstColumn.every((current) => azureArray.includes(current))) {
-      console.log("Azure is tha winna - firstColumn");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (secondColumn.every((current) => azureArray.includes(current))) {
-      console.log("Azure is tha winna - secondColumn");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (thirdColumn.every((current) => azureArray.includes(current))) {
-      console.log("Azure is tha winna - thirdColumn");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (
-      diagonalFromLeft.every((current) => azureArray.includes(current))
-    ) {
-      console.log("Azure is tha winna - diagonalFromLeft");
-      gameComplete = true;
-      playerTwoCounter++;
-    } else if (
+    if (
+      firstRow.every((current) => azureArray.includes(current)) ||
+      secondRow.every((current) => azureArray.includes(current)) ||
+      thirdRow.every((current) => azureArray.includes(current)) ||
+      firstColumn.every((current) => azureArray.includes(current)) ||
+      secondColumn.every((current) => azureArray.includes(current)) ||
+      thirdColumn.every((current) => azureArray.includes(current)) ||
+      diagonalFromLeft.every((current) => azureArray.includes(current)) ||
       diagonalFromRight.every((current) => azureArray.includes(current))
     ) {
-      console.log("Azure is tha winna - diagonalFromRight");
-      gameComplete = true;
+      console.log("Azure is tha winna");
+      // make sure only one point is given per round
+      if (azureArray.lengthh == 3 && !playerTwoPointGiven) {
+        console.log("azure lenght and no point given: ", azureArray.length);
+
+        playerTwoCounter++;
+        playerTwoPointGiven = true;
+        gameComplete = true;
+        console.log("azure point given: ", pointGiven);
+        // playerTwoPointGiven = true;
+      }
+      /*       if (gameComplete) {
+        return;
+      }
       playerTwoCounter++;
+      gameComplete = true; */
+      // square.style = "pointer-events: none";
+      /*       Array.from(squares).forEach((el) => {
+        // el.style = "pointer-events: none";
+        console.log("EL azre: ", el);
+        if (el.style == "background-color: pink") {
+          el.style = "background-color: pink";
+        } else if (el.style == "background-color: azure") {
+          el.style = "background-color: azure";
+        }
+        el.style = "pointer-events: none";
+      }); */
+      // Set color back to what it was
     }
 
     // Game over - no more
@@ -188,8 +224,11 @@ Array.from(squares).forEach((square, index) => {
       playerTwoCounterText.innerHTML = playerTwoCounter;
       // square.style = "pointer-events: none";
       // Array.from(squares).forEach((el) => (el.style = "pointer-events: none"));
+      // playerOnePointGiven = true;
+      // playerTwoPointGiven = true;
       console.log("We have a winner - change style");
       gameComplete = false;
+      pointGiven = false;
     }
   });
 });
